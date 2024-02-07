8b008fa2564103fcf88551f3604e83d319cee32e
justaCasualCoder <120528794+justaCasualCoder@users.noreply.github.com>
2024-02-07 13:46:18 -0500

    First Commit!

---
+++ README.md
@@ -0,0 +1,89 @@
+# Samsung Galaxy S5 AT&T Tethered Exploit
+
+# What does this do?
+
+It allows booting TWRP/AOSP ROM  by using [SVE-2016-7930](https://github.com/frederic/SVE-2016-7930) (Which uses buffer overflows) to run unsigned recovery/boot images from a uSD card. Read the [Full article](https://www.sstic.org/media/SSTIC2017/SSTIC-actes/attacking_samsung_secure_boot/SSTIC2017-Article-attacking_samsung_secure_boot-basse.pdf) for how it works in more detail.
+
+# :warning:This will  prevent your phone from booting unless you have a computer! This is a TETHERED Exploit! :warning:
+
+# Requirements
+
+- AT&T S5 on firmware version G900AUCS4DQB1
+
+- Micro SD card (:warning:This will be erased:warning:)
+
+- Heimdall with [This patch](https://github.com/frederic/SVE-2016-7930/blob/master/heimdall-increase_fileTransferSequenceMaxLength.patch) applied (a x64 pre-patched binary is in this repo)
+
+- A computer running Linux
+
+- TWRP image ([Download twrp-3.7.0_9-0-klte.img](https://dl.twrp.me/klte/twrp-3.7.0_9-0-klte.img.html)) named twrp.img in the repo dir (`wget --referer https://dl.twrp.me/klte/twrp-3.7.0_9-0-klte.img.html https://dl.twrp.me/klte/twrp-3.7.0_9-0-klte.img -O twrp.img`)
+
+# Getting started
+
+## Getting to the right firmware
+
+- Download [G900AUCS4DPH4](https://androidfilehost.com/?fid=312968873555011029) and flash it in odin
+
+- Download [PH4-QA1.zip](https://www.androidfilehost.com/?fid=745425885120714574) and [QA1-QB1.zip](https://www.androidfilehost.com/?fid=673368273298937968)
+
+- Flash both in recovery (Use ADB sideload from a computer or flash from uSD card)
+
+## Running it
+
+Get a Linux system to continue (Eg. a Ubuntu Desktop Live CD)
+
+- [Download this repo](https://github.com)
+
+- Extract it
+
+- Open a terminal in the folder you extracted it into
+
+- Install ADB (`sudo apt-get install android-sdk-platform-tools` on Ubuntu)
+
+- Put your uSD card into your phone
+
+- Run the script (`bash main.sh -p && bash main.sh -t` and follow the instructions to partition your uSD card and boot into TWRP)
+
+- When you poweroff the phone, boot it back into your ROM by running  `bash main.sh -b` and follow the instructions.
+
+# Script Usage
+
+```bash
+Usage: main.sh [-h] [-v] [-z ROM.zip] [-i boot.img]
+
+Bash script to boot AOSP ROM/TWRP on G900A
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+-t, --twrp       Flash/Boot TWRP
+-b, --boot      Do normal boot to LineageOS
+-z, --zip       Boot into TWRP and flash zip/boot image
+-i, --image     Flash custom boot image
+-p, --partition Partition uSD card. Run this once to set up your uSD card
+
+Example Usage:
+main.sh -z lineage-21.0-20240202-UNOFFICIAL-klte.zip
+main.sh -i boot.img
+```
+
+# Installing a ROM
+
+- Download your ROM of choice for `klte`
+
+- Move it into folder where you extracted the repo
+
+- Run `bash main.sh -z YOURROM.zip`
+  
+  - This will reboot into TWRP, Sideload the ZIP, and then reboot to Download mode to flash the new boot image.
+
+## Flash Magisk
+
+- Install Magisk
+
+- Copy the resulting `.img` to your computer (`adb pull /storage/emulated/0/Download/*.img .`)
+
+- Flash the new image (`bash main.sh -i magisk_patched.img`)
+
+
---
+++ main.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+# JustaCasualCoder 2024
+# Thanks so much to Frederic for making the PoC
+# https://github.com/frederic/SVE-2016-7930
+# Define variables to not break program
+TWRP=0
+BOOT=0
+LINEAGE=0
+FLASHZIP=0
+FLASHIMAGE=0
+PARTITION=0
+HEIMDALL_PATH="$(pwd)/heimdall"
+FILE=""
+REQFILE=("boot_sdcard.img" "boot_usb.img" "heimdall" "KLTE_USA_ATT.pit" "twrp.img")
+for file in "${REQFILE[@]}"; do
+    if [[ ! -f "$file" ]]; then
+        if [ "$file" == "boot_usb.img" ]; then
+        	echo "Extracting USB boot file..."
+        	gunzip boot_usb.img.gz
+        else
+        	echo "Failed to find needed file: $file"
+        	exit 1
+        fi
+    fi
+done
+usage() {
+cat <<EOF 
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-z ROM.zip] [-i boot.img]
+
+Bash script to boot AOSP ROM/TWRP on G900A
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+-t, --twrp   		Flash/Boot TWRP
+-b, --boot      Do normal boot to LineageOS
+-z, --zip       Boot into TWRP and flash zip/boot image
+-i, --image     Flash custom boot image
+-p, --partition Partition uSD card. Run this once to set up your uSD card
+
+Example Usage:
+$(basename "${BASH_SOURCE[0]}") -z lineage-21.0-20240202-UNOFFICIAL-klte.zip
+$(basename "${BASH_SOURCE[0]}") -i magisk_patched_boot.img
+EOF
+  exit
+}
+while :; do
+  case "${1-}" in
+  -h | --help) usage ;;
+  -v | --verbose) set -x ;;
+  -t | --twrp) TWRP=1 ;;
+  -b | --boot) BOOT=1 ;;
+	-p | --partition) PARTITION=1 ;;
+  -l | --lineage) LINEAGE=1 ;; 
+	-i | --image) FLASHIMAGE=1 
+	IMG_FILE=${2}
+	;;
+  -z | --zip) FLASHZIP=1 
+	FILE=${2}
+	;;
+  -?*) echo "Unknown option: $1" ;;
+  *) break ;;
+  esac
+  shift
+done
+heimdall() {
+	"$HEIMDALL_PATH" ${@#}
+}
+echo "Using $HEIMDALL_PATH"
+echo "Thanks so much to Frederic for making the PoC"
+echo "https://github.com/frederic/SVE-2016-7930"
+echo -e "\033[31mThis will wipe ALL DATA off your uSD card!\033[0m"
+echo -e "\033[31mPress enter to continue...\033[0m"
+read
+echo "Checking for files..."
+boot_usb.img.gz
+if [ ${PARTITION} = 1 ]; then
+	download_mode
+	heimdall flash --tflash --repartition --pit KLTE_USA_ATT.pit
+fi
+download_mode() {
+echo "Reboot to Download mode"
+echo "- Poweroff"
+echo "- Press and hold volume down, home, and power"
+echo "- Press volume up to accept"
+echo "Waiting for device..."
+heimdall detect &> /dev/null
+while [ $? -ne 0 ]; do
+	sleep 0.5
+	heimdall detect &> /dev/null
+done
+echo "Device detected"
+}
+TWRP_FLASH() {
+	download_mode
+	echo "Press and hold volume down and home"
+	echo "Press enter when ready"
+	read
+	heimdall flash --tflash  --RECOVERY twrp.img --BOOT boot_sdcard.img
+	echo "Waiting for device to reboot to Download mode..."
+	sleep 2
+	heimdall detect &> /dev/null
+	while [ $? -ne 0 ]; do
+		sleep 0.5
+		heimdall detect &> /dev/null
+	done
+	echo "Device detected"
+	echo "Triggering Exploit..."
+	heimdall flash --tflash --no-reboot --BOOT boot_usb.img
+	echo "Device should now boot to TWRP"
+	echo "Done"
+}
+if [ ${TWRP} = 1 ]; then
+	TWRP_FLASH
+fi
+if [ ${BOOT} = 1 ]; then
+	download_mode
+	echo "Device detected"
+	echo "Triggering Exploit..."
+	heimdall flash --tflash  --no-reboot --BOOT boot_usb.img
+	echo "Booting"
+	echo "Done"
+fi
+if [ ${FLASHZIP} = 1 ]; then
+	unzip -d /tmp/ ${FILE} boot.img 
+	TWRP_FLASH
+	echo "Waiting for TWRP..."
+	read
+	read
+	adb shell twrp sideload
+	sleep 5 # It take a while to go from shell to sideload
+	adb sideload ${FILE}
+	sleep 5 # It take a while to go from sideload to shell
+	adb shell twrp reboot download
+	echo "Waiting for device..."
+	heimdall detect &> /dev/null
+	while [ $? -ne 0 ]; do
+		sleep 0.5
+		heimdall detect &> /dev/null
+	done
+	echo "Device detected"
+	echo "Press and hold volume down and home"
+	echo "Press enter when ready"
+	read
+	heimdall flash --tflash  --RECOVERY /tmp/boot.img --BOOT boot_sdcard.img
+	echo "Waiting for device to reboot to Download mode..."
+	sleep 2
+	heimdall detect &> /dev/null
+	while [ $? -ne 0 ]; do
+		sleep 0.5
+		heimdall detect &> /dev/null
+	done
+	echo "Device detected"
+	echo "Triggering Exploit..."
+	heimdall flash --tflash  --no-reboot --BOOT boot_usb.img
+	echo "Booting ROM"
+	echo "Device should now boot to ROM"
+	echo "Done"
+fi
+if [ ${FLASHIMAGE} = 1 ]; then
+	download_mode
+	echo "Device detected"
+	echo "Press and hold volume down and home"
+	echo "Press enter when ready"
+	read
+	heimdall flash --tflash  --RECOVERY ${IMG_FILE} --BOOT boot_sdcard.img
+	echo "Waiting for device to reboot to Download mode..."
+	sleep 2
+	heimdall detect &> /dev/null
+	while [ $? -ne 0 ]; do
+		sleep 0.5
+		heimdall detect &> /dev/null
+	done
+	echo "Device detected"
+	echo "Triggering Exploit..."
+	heimdall flash --tflash  --no-reboot --BOOT boot_usb.img
+	echo "Booting ROM"
+	echo "Done"
+fi
